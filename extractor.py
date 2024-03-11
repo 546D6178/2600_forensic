c6e5e1f95e58345c6fcfc3a6afce15730d625b88
546D6178 <maxencebrondelle@gmail.com>
2024-03-11 18:49:12 +0100

    main

--- extractor.py
+++ extractor.py
@@ -1,22 +1,34 @@
 import re 
 import subprocess
-#import sys
+import sys
+
+def extract_offset(path_to_disk):
+
+    commande = f"mmls {path_to_disk}"
+
+    resultat_mmls = subprocess.check_output(commande, shell=True, text=True)
+
+    regex = r'^\d+:\s+\d+\s+(\d+)\s+\d+\s+\d+\s+Basic data partition'
+    correspondance = re.search(regex, resultat_mmls, re.MULTILINE)
+
+    if correspondance:
+        offset_start = correspondance.group(1)
+        print("Offset de start :", offset_start)
+    else:
+        print("Aucune correspondance trouvée.")
 
 
-fichier = "/mnt/share/Forensic_cours/ewf/disk/disk.E01" #input("Entrez le chemin du fichier : ")
 
-commande = f"mmls {fichier}"
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) == 0:
+        print("Utilisation: python script.py <path_to_disk>")
+        sys.exit(1)
 
-resultat_mmls = subprocess.check_output(commande, shell=True, text=True)
+    fichier = "/mnt/share/Forensic_cours/ewf/disk/disk.E01"
+    path_to_disk = fichier
+    #path_to_disk = sys.argv[1]
+    extract_offset(path_to_disk)
 
-regex = r'^\d+:\s+\d+\s+(\d+)\s+\d+\s+\d+\s+Basic data partition'
-correspondance = re.search(regex, resultat_mmls, re.MULTILINE)
-
-if correspondance:
-    offset_start = correspondance.group(1)
-    print("Offset de start :", offset_start)
-else:
-    print("Aucune correspondance trouvée.")
 
 #/mnt/share/Forensic_cours/ewf/disk/disk.E01
 
